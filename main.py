a423c6490bdaed43400105dbc4552236be469632
moforemmanuel <moforemmanuel1000@gmail.com>
2021-05-22 22:26:59 +0100

    included POST method to endpoints

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ from flask import Flask,request,render_template,url_for
 template_dir = os.path.abspath('/home/manuel/Desktop/project/calculator_with_ui/views')
 app = Flask(__name__, template_folder = template_dir)
 
-@app.route('/')
+@app.route('/', methods = 'POST')
 def index():
     return render_template("index.html")
 
-@app.route('/basic_operations')
+@app.route('/basic_operations', methods = 'POST')
 def basic_operations():
     return None
 
-@app.route('/trigonometric_operations')
+@app.route('/trigonometric_operations', methods = 'POST')
 def trigonometric_operations():
     return None
 
-@app.route('/hyperbolic_operations')
+@app.route('/hyperbolic_operations', methods = 'POST')
 def hyperbolic_operations():
     return None
 
